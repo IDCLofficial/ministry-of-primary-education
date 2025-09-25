d12faed6d63db1d7926343979834828e7b69b0a8
fabiconcept <favourajokubi@gmail.com>
2025-09-25 13:39:38 +0100

    feat: hide GlobalLoader and Navbar components on portal page

--- src/components/GlobalLoader.jsx
+++ src/components/GlobalLoader.jsx
@@ -27,6 +27,12 @@ export default function GlobalLoader() {
     return () => clearTimeout(timeout);
   }, [pathname]);
 
+  console.log(pathname);
+
+  if (pathname === '/portal') {
+    return null;
+  }
+
   if (loading || isRouteChanging) {
     return (
       <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-gradient-to-br from-[rgba(1,50,32,0.9)] via-[rgba(0,20,10,0.7)] to-transparent
