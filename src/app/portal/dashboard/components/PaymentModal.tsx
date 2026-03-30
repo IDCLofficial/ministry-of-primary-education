95e1f857d16ea58c0e878621d179e5860764b1bf
fabiconcept <favourajokubi@gmail.com>
2026-03-30 13:00:10 +0100

    fix: enable payment maintenance mode in PaymentModal component
    - Updated PAYMENT_MAINTENANCE_MODE to true, indicating that the payment system is currently under maintenance. This change will help manage user expectations during this period.

--- src/app/portal/dashboard/components/PaymentModal.tsx
+++ src/app/portal/dashboard/components/PaymentModal.tsx
@@ -10,7 +10,7 @@ if (process.env.NEXT_PUBLIC_MODE === undefined){
   throw new Error("Missing important variable: NEXT_PUBLIC_MODE") 
 }
 
-const PAYMENT_MAINTENANCE_MODE = false;
+const PAYMENT_MAINTENANCE_MODE = true;
 
 const MAINTENANCE_ETA: string | null = "2026-03-24T17:00:00Z"
 
